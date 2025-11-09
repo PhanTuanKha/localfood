7a7d434f88c29e3577ff263d0f47501a9ba9653a
lebinhantd-glitch <lebinhantd@gmail.com>
2025-11-09 11:23:03 +0700

    thanh bar

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,50 +1,73 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header', 
-  imports: [], 
+  imports: [CommonModule], 
   templateUrl: './header.html', 
   styleUrl: './header.css', })
-export class Header implements OnInit {
+export class Header implements OnInit, OnDestroy {
   location: string = 'Đang tải vị trí...';
   loading: boolean = true;
-
+  dropdownOpen: boolean = false;
+  activeMenu: string = '';
+  isLoggedIn: boolean = false;
   constructor(private ngZone: NgZone) {}
-
-  ngOnInit(): void {
-    this.getUserLocation();
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
   }
 
+  selectMenu(menu: string) {
+    this.activeMenu = menu;
+    if (menu === 'Đăng nhập') {
+      this.isLoggedIn = true;
+    }
+    if (menu === 'Đăng xuất') {
+      this.isLoggedIn = false;
+    }
+  }
+  ngOnInit(): void {
+    this.getUserLocation();
+    document.addEventListener('click', (event: any) => {
+      const inside = event.target.closest('.dropdown-wrapper');
+      if (!inside) {
+        this.ngZone.run(() => (this.dropdownOpen = false));
+      }
+    });
+  }
   getUserLocation(): void {
     if (!navigator.geolocation) {
       this.location = 'Trình duyệt không hỗ trợ định vị';
       this.loading = false;
       return;
     }
-
     let timeoutReached = false;
-
     const timeout = setTimeout(() => {
-    timeoutReached = true;
-    this.ngZone.run(() => {
-      this.location = 'Không thể xác định vị trí';
-      this.loading = false;
-    });
-  }, 15000);
-
+      timeoutReached = true;
+      this.ngZone.run(() => {
+        this.location = 'Không thể xác định vị trí';
+        this.loading = false;
+      });
+    }, 15000);
     navigator.geolocation.getCurrentPosition(
       (position) => {
         if (timeoutReached) return;
         clearTimeout(timeout);
-
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-
-        fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`, {
-          headers: { 'User-Agent': 'AngularApp/1.0 (youremail@example.com)' },
-        })
-          .then(res => res.json())
-          .then(data => {
+        fetch(
+          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`,
+          {
+            headers: {
+              'User-Agent': 'AngularApp/1.0 (youremail@example.com)',
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
             this.ngZone.run(() => {
               this.location =
                 data.address?.city ||

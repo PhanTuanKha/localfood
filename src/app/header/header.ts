d755a43f83c80ce688813a3237b5fcd316941255
lebinhantd-glitch <lebinhantd@gmail.com>
2025-11-09 10:19:34 +0700

    update location

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, NgZone } from '@angular/core';
   styleUrl: './header.css', })
 export class Header implements OnInit {
   location: string = 'Đang tải vị trí...';
+  loading: boolean = true;
 
   constructor(private ngZone: NgZone) {}
 
@@ -17,21 +18,33 @@ export class Header implements OnInit {
   getUserLocation(): void {
     if (!navigator.geolocation) {
       this.location = 'Trình duyệt không hỗ trợ định vị';
+      this.loading = false;
       return;
     }
 
+    let timeoutReached = false;
+
+    const timeout = setTimeout(() => {
+    timeoutReached = true;
+    this.ngZone.run(() => {
+      this.location = 'Không thể xác định vị trí';
+      this.loading = false;
+    });
+  }, 15000);
+
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        const lat: number = position.coords.latitude;
-        const lon: number = position.coords.longitude;
+        if (timeoutReached) return;
+        clearTimeout(timeout);
+
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
 
         fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`, {
-          headers: {
-            'User-Agent': 'AngularApp/1.0 (example@email.com)',
-          },
+          headers: { 'User-Agent': 'AngularApp/1.0 (youremail@example.com)' },
         })
-          .then((response) => response.json())
-          .then((data: any) => {
+          .then(res => res.json())
+          .then(data => {
             this.ngZone.run(() => {
               this.location =
                 data.address?.city ||
@@ -39,20 +52,25 @@ export class Header implements OnInit {
                 data.address?.village ||
                 data.display_name ||
                 'Không xác định được vị trí';
+              this.loading = false;
             });
           })
           .catch(() => {
             this.ngZone.run(() => {
               this.location = 'Không thể lấy thông tin vị trí';
+              this.loading = false;
             });
           });
       },
       (error) => {
+        clearTimeout(timeout);
         console.error('Lỗi định vị:', error);
         this.ngZone.run(() => {
           this.location = 'Quyền truy cập vị trí bị từ chối';
+          this.loading = false;
         });
-      }
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 0 }
     );
   }
 }

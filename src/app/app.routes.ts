5aa9d45a0fb571f0df92b351f987ade23e88f383
lebinhantd-glitch <lebinhantd@gmail.com>
2025-11-12 22:01:06 +0700

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Signin } from './auth/sigin/sigin';
 import { Register } from './auth/register/register';
 import { ForgotPasswordComponent } from './auth/forgot-pasword/forgot-pasword';
+import { Mainpage } from './order/mainpage/mainpage';
 
 export const routes: Routes = [{
-    path: '', redirectTo: 'signin', pathMatch: 'full'},
+    path: '', redirectTo: 'mainpage', pathMatch: 'full'},
     {path: 'signin', component: Signin},
     {path: 'register', component: Register},
-    {path: 'forgot-password', component: ForgotPasswordComponent}
+    {path: 'forgot-password', component: ForgotPasswordComponent},
+    {path: 'mainpage', component: Mainpage}
 ];
--- src/app/order/mainpage/mainpage.ts
+++ src/app/order/mainpage/mainpage.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-mainpage',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.css',
 })
 export class Mainpage {
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLDivElement>;
 
+  ngAfterViewInit(){
+
+  }
+  scrollLeft(){
+    const container=this.scrollContainer.nativeElement;
+    if (container.scrollLeft <=0){
+      container.scrollTo({
+        left: container.scrollWidth,
+        behavior: 'smooth'
+      })
+    } else {
+      container.scrollBy({
+        left: -300,
+        behavior: 'smooth'
+      })
+    }
+  }
+  scrollRight() {
+    const container = this.scrollContainer.nativeElement
+    
+    const maxScrollLeft = container.scrollWidth - container.clientWidth
+    if (container.scrollLeft >= maxScrollLeft - 5) {
+      container.scrollTo({
+        left: 0,
+        behavior: 'smooth',
+      });
+    } else {
+      container.scrollBy({
+        left: 300,
+        behavior: 'smooth',
+      })
+    }
+  }
 }
